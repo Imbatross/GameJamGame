5076c93c4fd409eab17d146a09ff62308e4edd68
Devin <devinpquinn4@gmail.com>
2020-02-01 12:49:25 -0500

    Robot movement setup

--- Assets/DevinScripts/Robot.cs
+++ Assets/DevinScripts/Robot.cs
@@ -4,12 +4,38 @@ using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
-    public bool player1;
-    private bool activated = false;
-    private int speed;
+    [Range(1,2)]
+    public int player = 1;
+    public bool activated = false;
+    private bool isFighting = false;
+    private float speed = 1;
+    private Vector2 movement;
+    private Rigidbody2D rb;
     private int hp;
     private Upgrade myUpgrade;
 
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        movement = new Vector2(0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        if(activated && !isFighting)
+        {
+            movement.x = speed;
+        }
+        else
+        {
+            movement.x = 0;
+        }
+        if(player == 2)
+        {
+            movement.x *= -1;
+        }
+        rb.velocity = movement;
+    }
 
     public void Activate()
     {
@@ -41,7 +67,7 @@ public class Robot : MonoBehaviour
         myUpgrade = upg;
     }
 
-    public void SetSpeed(int spd)
+    public void SetSpeed(float spd)
     {
         speed = spd;
     }
