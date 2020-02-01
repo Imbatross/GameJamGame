743b5337bcc0f67f77bfa342dd5f37e6dfe0434e
Devin <devinpquinn4@gmail.com>
2020-02-01 12:03:44 -0500

    Beginning Robot and Upgrade base class setup

---
+++ Assets/DevinScripts/Robot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    public bool player1;
+    private bool activated = false;
+    private int speed;
+    private int hp;
+    private Upgrade myUpgrade;
+
+
+    public void Activate()
+    {
+        activated = true;
+    }
+
+    private void Death()
+    {
+
+    }
+
+    private void TakeDamage(int dmg)
+    {
+        hp -= dmg;
+        if(hp <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void AddHealth(int add)
+    {
+        hp += add;
+    }
+
+    public void AddUpgrade(Upgrade upg)
+    {
+        upg.SetRobot(this);
+        myUpgrade = upg;
+    }
+
+    public void SetSpeed(int spd)
+    {
+        speed = spd;
+    }
+
+}
---
+++ Assets/DevinScripts/Upgrade.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : MonoBehaviour
+{
+    private Robot myRobot;
+
+    public void SetRobot(Robot rbt)
+    {
+        myRobot = rbt;
+    }
+}
